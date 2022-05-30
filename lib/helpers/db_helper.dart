98576bb82c5f686a4d76bdb6e4554fbbf3cdfa77
Saul <phasecross@hotmail.com>
2022-05-30 14:30:42 -0700

    Added a database helper for SQFlite

---
+++ lib/helpers/db_helper.dart
@@ -0,0 +1,47 @@
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqlite_api.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+
+class DBHelper {
+  static Future<Database> database() async {
+    final dbPath = await sql.getDatabasesPath();
+    return sql.openDatabase(path.join(dbPath, 'posts.db'),
+        onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE posts(post_id TEXT PRIMARY KEY, post_text TEXT, post_date TEXT, post_emotions TEXT)');
+    }, version: 1);
+  }
+
+  static Future<List<Map<String, dynamic>>> fetchData(String table) async {
+    final db = await DBHelper.database();
+    return db.query(table);
+  }
+
+  static Future<int> insert(String table, Map<String, dynamic> data) async {
+    final db = await DBHelper.database();
+    return await db.insert(
+      table,
+      data,
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  static Future<int> update(String table, Map<String, dynamic> data) async {
+    final db = await DBHelper.database();
+    return await db.update(
+      table,
+      data,
+      where: 'id = ?',
+      whereArgs: [data['id']],
+    );
+  }
+
+  static Future<int> delete(String table, String id) async {
+    final db = await DBHelper.database();
+    return await db.delete(
+      table,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}

7613da1431b27b7da4e4ff3cb86c802c28e17259
Saul <phasecross@hotmail.com>
2022-06-02 14:49:05 -0700

    Converted to StatelessWidget

--- lib/widgets/posts/post_details_emotions_list.dart
+++ lib/widgets/posts/post_details_emotions_list.dart
@@ -3,26 +3,23 @@ import 'package:flutter/material.dart';
 import '../../providers/emotion.dart';
 import 'post_details_emotion_item.dart';
 
-class PostDetailsEmotionsList extends StatefulWidget {
+class PostDetailsEmotionsList extends StatelessWidget {
   final List<Emotion> postEmotions;
 
   const PostDetailsEmotionsList(this.postEmotions, {Key? key})
       : super(key: key);
 
   @override
-  State<PostDetailsEmotionsList> createState() =>
-      _PostDetailsEmotionsListState();
-}
-
-class _PostDetailsEmotionsListState extends State<PostDetailsEmotionsList> {
-  @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: widget.postEmotions.length,
+      itemCount: postEmotions.length,
       itemBuilder: (ctx, index) {
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-          child: PostEmotionItem(widget.postEmotions[index]),
+          child: PostDetailsEmotionItem(
+            postEmotions[index],
+            key: ValueKey(postEmotions[index].name),
+          ),
         );
       },
     );

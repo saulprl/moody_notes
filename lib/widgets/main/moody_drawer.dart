48afc7608d5be85dab8e97e33948fe1d8e5f3d18
Saul <phasecross@hotmail.com>
2022-06-06 13:54:44 -0700

    Added drawer buttons

--- lib/widgets/main/moody_drawer.dart
+++ lib/widgets/main/moody_drawer.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../../screens/calendar_screen.dart';
+
 class MoodyDrawer extends StatelessWidget {
   const MoodyDrawer({Key? key}) : super(key: key);
+
+  Widget _buildButton(String title, IconData icon, VoidCallback tapHandler) {
+    return ListTile(
+      leading: Icon(icon, size: 26),
+      title: Text(
+        title,
+        style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+      onTap: tapHandler,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +32,17 @@ class MoodyDrawer extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 20.0),
+          _buildButton(
+            'Inicio',
+            Icons.home,
+            () => Navigator.of(context).pushReplacementNamed('/'),
+          ),
+          _buildButton(
+            'Calendario',
+            Icons.calendar_month,
+            () => Navigator.of(context)
+                .pushReplacementNamed(CalendarScreen.routeName),
+          ),
         ],
       ),
     );

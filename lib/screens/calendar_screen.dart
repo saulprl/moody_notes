631304b36671ab21a3d8af205718b2e3df2044de
Saul <phasecross@hotmail.com>
2022-06-07 13:59:14 -0700

    Added a PostsList managed by the selected day and properly set TableCalendar up

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -4,13 +4,38 @@ import 'package:table_calendar/table_calendar.dart';
 // import 'package:intl/intl.dart';
 
 import '../providers/posts.dart';
+import '../widgets/posts/post_list.dart';
 import '../widgets/main/moody_drawer.dart';
 import '../widgets/emotions/color_bullet.dart';
 
-class CalendarScreen extends StatelessWidget {
+class CalendarScreen extends StatefulWidget {
   static const routeName = '/calendar-screen';
 
   const CalendarScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  var _calendarFormat = CalendarFormat.month;
+  late DateTime _selectedDay;
+  late DateTime _focusedDay;
+  late int _notesPerDay;
+  var _isInit = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (!_isInit) {
+      _selectedDay = DateTime.now();
+      _notesPerDay = Provider.of<Posts>(
+        context,
+        listen: false,
+      ).countPosts(_selectedDay);
+      _isInit = true;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +50,6 @@ class CalendarScreen extends StatelessWidget {
       body: Column(
         children: [
           TableCalendar(
-            onFormatChanged: (format) {},
             calendarBuilders: CalendarBuilders(
               markerBuilder: (ctx, day, focusedDay) {
                 final opacity = postsData.calculateOpacity(day);
@@ -38,7 +62,9 @@ class CalendarScreen extends StatelessWidget {
                 return Stack(
                   children: [
                     Container(
-                      color: Colors.pink.withOpacity(opacity),
+                      color: _selectedDay == day
+                          ? Colors.purple.withOpacity(opacity)
+                          : Colors.pink.withOpacity(opacity),
                       child: Center(child: Text(day.day.toString())),
                     ),
                     Padding(
@@ -58,10 +84,50 @@ class CalendarScreen extends StatelessWidget {
               },
             ),
             locale: 'es_MX',
-            firstDay: DateTime.utc(2010, 10, 16),
-            lastDay: DateTime.utc(2030, 10, 16),
-            focusedDay: DateTime.now(),
+            firstDay: DateTime.utc(2020, 8, 14),
+            lastDay: DateTime.now().add(const Duration(days: 365)),
+            calendarFormat: _calendarFormat,
+            focusedDay: _selectedDay,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (day, focusedDay) {
+              setState(() {
+                _selectedDay = day;
+                _focusedDay = focusedDay;
+                _notesPerDay = postsData.countPosts(day);
+              });
+            },
+            onFormatChanged: (format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            },
+            availableCalendarFormats: const {
+              CalendarFormat.month: 'Mes',
+              CalendarFormat.twoWeeks: 'Dos semanas',
+              CalendarFormat.week: 'Semana',
+            },
           ),
+          const Divider(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text('Notas del día: $_notesPerDay'),
+                ),
+                // ElevatedButton.icon(
+                //   icon: const Icon(Icons.search),
+                //   label: const Text('Buscar notas'),
+                //   onPressed: () {},
+                // ),
+              ],
+            ),
+          ),
+          PostList(date: _selectedDay),
         ],
       ),
     );

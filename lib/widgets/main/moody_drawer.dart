6222565093a6fbd2d399bc33089775a9e959424e
Saul <phasecross@hotmail.com>
2022-06-27 15:34:57 -0700

    Added a Settings button and modified About button to pushNamed instead of replace. Also added a theme switch

--- lib/widgets/main/moody_drawer.dart
+++ lib/widgets/main/moody_drawer.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+
+import '../../providers/theme_preferences.dart';
 
 import '../../screens/about_screen.dart';
 import '../../screens/calendar_screen.dart';
+import '../../screens/settings_screen.dart';
 
 class MoodyDrawer extends StatelessWidget {
   const MoodyDrawer({Key? key}) : super(key: key);
@@ -20,44 +24,61 @@ class MoodyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: [
-          AppBar(
-            automaticallyImplyLeading: false,
-            backgroundColor: Theme.of(context).colorScheme.primary,
-            title: const Text('Moody Notes'),
-          ),
-          // Container(
-          //   height: 120,
-          //   width: double.infinity,
-          //   padding: const EdgeInsets.all(20.0),
-          //   alignment: Alignment.centerLeft,
-          //   child: const Text(
-          //     'Moody Notes',
-          //     style: TextStyle(fontSize: 30.0),
-          //   ),
-          // ),
-          const SizedBox(height: 10.0),
-          _buildButton(
-            'Inicio',
-            Icons.home,
-            () => Navigator.of(context).pushReplacementNamed('/'),
-          ),
-          _buildButton(
-            'Calendario',
-            Icons.calendar_month,
-            () => Navigator.of(context)
-                .pushReplacementNamed(CalendarScreen.routeName),
-          ),
-          _buildButton('Acerca de', Icons.info, () async {
-            PackageInfo info = await PackageInfo.fromPlatform();
-            Navigator.of(context).pushReplacementNamed(
-              AboutScreen.routeName,
-              arguments: info.version,
-            );
-          }),
-        ],
+    return Consumer<ThemePreferences>(
+      builder: (context, themeNotifier, child) => Drawer(
+        child: Column(
+          children: [
+            AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              title: const Text('Moody Notes'),
+              actions: [
+                IconButton(
+                  icon: Icon(
+                    themeNotifier.isDark ? Icons.light_mode : Icons.dark_mode,
+                  ),
+                  onPressed: () => themeNotifier.isDark
+                      ? themeNotifier.isDark = false
+                      : themeNotifier.isDark = true,
+                ),
+              ],
+            ),
+            // Container(
+            //   height: 120,
+            //   width: double.infinity,
+            //   padding: const EdgeInsets.all(20.0),
+            //   alignment: Alignment.centerLeft,
+            //   child: const Text(
+            //     'Moody Notes',
+            //     style: TextStyle(fontSize: 30.0),
+            //   ),
+            // ),
+            const SizedBox(height: 10.0),
+            _buildButton(
+              'Inicio',
+              Icons.home,
+              () => Navigator.of(context).pushReplacementNamed('/'),
+            ),
+            _buildButton(
+              'Calendario',
+              Icons.calendar_month,
+              () => Navigator.of(context)
+                  .pushReplacementNamed(CalendarScreen.routeName),
+            ),
+            _buildButton(
+              'Ajustes',
+              Icons.settings,
+              () => Navigator.of(context).pushNamed(SettingsScreen.routeName),
+            ),
+            _buildButton('Acerca de', Icons.info, () async {
+              PackageInfo info = await PackageInfo.fromPlatform();
+              Navigator.of(context).pushNamed(
+                AboutScreen.routeName,
+                arguments: info.version,
+              );
+            }),
+          ],
+        ),
       ),
     );
   }

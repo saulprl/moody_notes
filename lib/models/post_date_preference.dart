b92ad6584665fa29952424f71986dcc60fe2aaaf
Saul <phasecross@hotmail.com>
2022-06-27 17:00:39 -0700

    Reformatted PREF_KEY to lowerCamelCase

--- lib/models/post_date_preference.dart
+++ lib/models/post_date_preference.dart
@@ -1,15 +1,15 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PostDatePreference {
-  static const PREF_KEY = 'ask_for_date';
+  static const prefKey = 'ask_for_date';
 
   setPreference(bool value) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    sharedPreferences.setBool(PREF_KEY, value);
+    sharedPreferences.setBool(prefKey, value);
   }
 
   Future<bool> getPreference() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    return sharedPreferences.getBool(PREF_KEY) ?? false;
+    return sharedPreferences.getBool(prefKey) ?? false;
   }
 }

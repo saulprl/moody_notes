0c56b14b288933b07950173cadb2cf31d2a2ca81
Saul <phasecross@hotmail.com>
2022-06-27 17:05:18 -0700

    Removed unnecesary comments to improve code readability

--- lib/widgets/emotions/emotion_item.dart
+++ lib/widgets/emotions/emotion_item.dart
@@ -3,19 +3,14 @@ import 'package:provider/provider.dart';
 
 import '../../providers/emotion.dart';
 import '../../providers/emotions.dart';
+
 import '../main/linked_label_checkbox.dart';
 
 class EmotionItem extends StatefulWidget {
-  // final String title;
-  // final Color color;
-  // final bool selected;
   final void Function(String args)? onTap;
 
   const EmotionItem({
     Key? key,
-    // required this.title,
-    // required this.color,
-    // required this.selected,
     this.onTap,
   }) : super(key: key);
 
@@ -40,38 +35,9 @@ class _EmotionItemState extends State<EmotionItem> {
       value: emotion.isSelected,
       color: emotion.color,
       onChanged: (value) {
-        // setState(() {
-        // emotion.toggleSelected();
-        // widget.updateEmotions!(widget.title, value, null);
-        // });
         emotionData.setSelected(emotion, value);
       },
       onTap: widget.onTap,
     );
-
-    // return CheckboxListTile(
-    //   value: _selected,
-    //   selected: _selected,
-    //   checkColor: widget.color,
-    //   tileColor: widget.color,
-    //   selectedTileColor: widget.color,
-    //   activeColor: Colors.white,
-    //   title: Text(
-    //     widget.title,
-    //     // textAlign: TextAlign.center,
-    //     style: const TextStyle(
-    //       fontSize: 16.0,
-    //       fontWeight: FontWeight.bold,
-    //     ),
-    //   ),
-    //   onChanged: (value) {
-    //     setState(() {
-    //       _selected = value!;
-    //     });
-    //   },
-    //   shape: RoundedRectangleBorder(
-    //     borderRadius: BorderRadius.circular(16.0),
-    //   ),
-    // );
   }
 }

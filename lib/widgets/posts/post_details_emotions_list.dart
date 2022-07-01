63faa8c32d40ad0184e6be375581b4d8057400f2
Saul <phasecross@hotmail.com>
2022-07-01 15:05:18 -0700

    Optimized imports and changed List of Emotions for a whole Post in favor of responsiveness

--- lib/widgets/posts/post_details_emotions_list.dart
+++ lib/widgets/posts/post_details_emotions_list.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 
-import '../../providers/emotion.dart';
-import 'post_details_emotion_item.dart';
+import '../../models/post.dart';
+
+import './post_details_emotion_item.dart';
 
 class PostDetailsEmotionsList extends StatelessWidget {
-  final List<Emotion> postEmotions;
+  final Post post;
 
-  const PostDetailsEmotionsList(this.postEmotions, {Key? key})
-      : super(key: key);
+  const PostDetailsEmotionsList(this.post, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: postEmotions.length,
+      itemCount: post.emotions.length,
       itemBuilder: (ctx, index) {
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
           child: PostDetailsEmotionItem(
-            postEmotions[index],
-            key: ValueKey(postEmotions[index].name),
+            post.emotions[index],
+            key: ValueKey(post.emotions[index].name),
           ),
         );
       },
